6ba486b246a27825f739086c300237021eee7a7e
Maksym <makkov2705@gmail.com>
2021-11-01 23:10:36 +0300

    (refactor) Refactor difficulty loaders

--- Game/src/main/java/Base/MapLoaders/HardDifficultyLoader.java
+++ Game/src/main/java/Base/MapLoaders/HardDifficultyLoader.java
@@ -8,45 +8,43 @@ import java.util.Random;
 
 public class HardDifficultyLoader implements DifficultyLoader {
 
+    short goldCount = 5;
+    short botCount = 10;
+    final String[] array = {"N", "N", "N", "N", "GG", "M", "M", "N", "N", "N"};
+    Coordinate playerCoordinate = new Coordinate(5, 6);
+
     @Override
-    public AbstractFigur[][] loading(AbstractFigur [][] data, Player player){
-        final String[] array = {"N","N","N","N","GG","M","M","N","N","N"};
-        short goldCount = 5;
-        short botCount = 10;
+    public AbstractFigur[][] loading(AbstractFigur[][] data, Player player) {
         AbstractFigur exit = Exit.getExit();
         Random random = new Random();
         int exitIndexX = random.nextInt(11);
         int exitIndexY = random.nextInt(10);
         data[exitIndexY][exitIndexX] = exit;
+        player = new Player();
+        player.setCoordinate(playerCoordinate);
+        data[playerCoordinate.getY()][playerCoordinate.getX()] = player;
         for (int i = 0; i < data.length; i++) {
             for (int j = 0; j < data[i].length; j++) {
                 int elemIndex = random.nextInt(array.length);
-                if(data[i][j].getClass() == Emptiness.class) {
+                if (data[i][j].getClass() == Emptiness.class) {
                     AbstractFigur arrayValue;
-                    if (array[elemIndex].equals("M") && botCount > 0){
+                    if (array[elemIndex].equals("M") && botCount > 0) {
                         arrayValue = new Bot();
                         botCount--;
-                    }
-                    else if(array[elemIndex].equals("GG") && goldCount > 0){
+                    } else if (array[elemIndex].equals("GG") && goldCount > 0) {
                         arrayValue = new Gold();
                         goldCount--;
-                    }
-                    else {
+                    } else {
                         arrayValue = data[i][j];
                     }
                     arrayValue.setCoordinate(new Coordinate(j, i));
-                    data[i][j]=arrayValue;
+                    data[i][j] = arrayValue;
                 }
 
             }
 
         }
-        int playerX = 5;
-        int playerY = 6;
-        player = new Player();
-        player.setCoordinate(new Coordinate(playerX, playerY));
 
-        data[playerY][playerX] = player;
         return data;
     }
 }
--- Game/src/main/java/Base/UserInterface/GameMap.java
+++ Game/src/main/java/Base/UserInterface/GameMap.java
@@ -49,7 +49,7 @@ public class GameMap extends JPanel implements CollectionSubscriber, KeyListener
 
     public GameMap() {
         LoaderFactory loaderFactory = new LoaderFactory();
-        DifficultyLoader difficultyLoader = loaderFactory.getLoader(DifficultyLoaderType.EASYLOADER);
+        DifficultyLoader difficultyLoader = loaderFactory.getLoader(DifficultyLoaderType.HARDLOADER);
         collection = new ArrayCollection(difficultyLoader);
         collection.addListener(this);
         JFrame frame = new JFrame("Maze runner");
