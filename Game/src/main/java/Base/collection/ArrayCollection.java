d05e1725332f7f1c3a6bd1c7ac1b30b86acfd721
Pashhaa <71789749+Pashhaa@users.noreply.github.com>
2023-06-13 21:48:37 +0300

    (feature) unified css

--- Game/src/main/java/Base/collection/ArrayCollection.java
+++ Game/src/main/java/Base/collection/ArrayCollection.java
@@ -1,6 +1,7 @@
 package Base.collection;
 
 //import Base.db.PlayerDAO;
+
 import Base.mapLoaders.DifficultyLoader;
 import Base.mapLoaders.mapLoaders.MapLoaderFactory;
 import Base.mapLoaders.mapLoaders.Maps;
@@ -21,165 +22,165 @@ import java.util.Objects;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class ArrayCollection extends CollectionPublisherImpl implements GameCollection {
-//    private final PlayerDAO playerDAO = new PlayerDAO();
+    //    private final PlayerDAO playerDAO = new PlayerDAO();
     private AbstractFigur[][] data;
-    private final List<AbstractMovingFigur> movingObjects = new CopyOnWriteArrayList<>();
-    private Player player = new Player();
-    private final List<Bullet> bulletList = new CopyOnWriteArrayList<>();
+    private final List < AbstractMovingFigur > movingObjects = new CopyOnWriteArrayList <>( );
+    private Player player = new Player( );
+    private final List < Bullet > bulletList = new CopyOnWriteArrayList <>( );
 
-    public ArrayCollection(DifficultyLoader difficultyLoader) throws Exception {
-        MapLoaderFactory mapLoaderFactory = new MapLoaderFactory();
-        data = mapLoaderFactory.getMap(Maps.TETRIS);
-        data = difficultyLoader.loading(this.data, player);
-        initOthers();
+    public ArrayCollection( DifficultyLoader difficultyLoader ) throws Exception {
+        MapLoaderFactory mapLoaderFactory = new MapLoaderFactory( );
+        data = mapLoaderFactory.getMap( Maps.TETRIS );
+        data = difficultyLoader.loading( this.data , player );
+        initOthers( );
     }
 
-    private void initOthers() {
-        for (AbstractFigur[] datum : data) {
-            for (int j = 0; j < datum.length; j++) {
-                if (datum[j].getObjectType() == ObjectType.PLAYER) {
-                    player = (Player) datum[j];
+    private void initOthers( ) {
+        for ( AbstractFigur[] datum : data ) {
+            for ( int j = 0 ; j < datum.length ; j++ ) {
+                if ( datum[ j ].getObjectType( ) == ObjectType.PLAYER ) {
+                    player = ( Player ) datum[ j ];
                 }
-                if (datum[j] instanceof AbstractMovingFigur) {
-                    movingObjects.add((AbstractMovingFigur) datum[j]);
+                if ( datum[ j ] instanceof AbstractMovingFigur ) {
+                    movingObjects.add( ( AbstractMovingFigur ) datum[ j ] );
                 }
             }
         }
     }
 
     @Override
-    public AbstractFigur[][] getData() {
+    public AbstractFigur[][] getData( ) {
         return data;
     }
 
-    public AbstractFigur getFigurByCoordinate(Coordinate coordinate) {
+    public AbstractFigur getFigurByCoordinate( Coordinate coordinate ) {
         try {
-            return data[coordinate.getY()][coordinate.getX()];
-        } catch (NullPointerException | ArrayIndexOutOfBoundsException e) {
+            return data[ coordinate.getY( ) ][ coordinate.getX( ) ];
+        } catch ( NullPointerException | ArrayIndexOutOfBoundsException e ) {
             return null;
         }
     }
 
     @Override
-    public void setObjectByCoordinate(int y, int x, AbstractFigur object) {
+    public void setObjectByCoordinate( int y , int x , AbstractFigur object ) {
         try {
-            object.setCoordinate(new Coordinate(x, y));
-            data[y][x] = object;
-        }catch (NullPointerException ignored){
+            object.setCoordinate( new Coordinate( x , y ) );
+            data[ y ][ x ] = object;
+        } catch ( NullPointerException ignored ) {
         }
     }
 
     @Override
-    public void shoot (ObjectType type, Direction direction) {
-        for (AbstractMovingFigur movingFigur : movingObjects) {
-            if (movingFigur.getObjectType() == type) {
-                shootToDirection (movingFigur, direction);
+    public void shoot( ObjectType type , Direction direction ) {
+        for ( AbstractMovingFigur movingFigur : movingObjects ) {
+            if ( movingFigur.getObjectType( ) == type ) {
+                shootToDirection( movingFigur , direction );
             }
         }
     }
 
-    private void shootToDirection (AbstractMovingFigur movingFigur, Direction direction) {
-        int[] nextCoord = movingFigur.move(direction);
-        int y = nextCoord[0];
-        int x = nextCoord[1];
-        AbstractFigur nextObject = getFigurByCoordinate(new Coordinate(x, y));
-        Action action = movingFigur.process(nextObject);
-        if (action == Action.MOVE) {
-            Bullet bullet = new Bullet();
-            bullet.setCoordinate(player.getCoordinate());
-            bullet.setShootingDirection(direction);
-            bulletList.add(bullet);
+    private void shootToDirection( AbstractMovingFigur movingFigur , Direction direction ) {
+        int[] nextCoord = movingFigur.move( direction );
+        int y = nextCoord[ 0 ];
+        int x = nextCoord[ 1 ];
+        AbstractFigur nextObject = getFigurByCoordinate( new Coordinate( x , y ) );
+        Action action = movingFigur.process( nextObject );
+        if ( action == Action.MOVE ) {
+            Bullet bullet = new Bullet( );
+            bullet.setCoordinate( player.getCoordinate( ) );
+            bullet.setShootingDirection( direction );
+            bulletList.add( bullet );
         }
     }
 
     @Override
-    public Player getPlayer() {
+    public Player getPlayer( ) {
         return player;
     }
 
     @Override
-    public void moveMovableFigur(ObjectType type, Direction direction) {
-        for (AbstractMovingFigur movingFigur : movingObjects) {
-            if (movingFigur.getObjectType() == type) {
-                moveFigurToDirection(movingFigur, direction);
+    public void moveMovableFigur( ObjectType type , Direction direction ) {
+        for ( AbstractMovingFigur movingFigur : movingObjects ) {
+            if ( movingFigur.getObjectType( ) == type ) {
+                moveFigurToDirection( movingFigur , direction );
             }
         }
     }
 
     @Override
-    public void moveMovableFigur(ObjectType type, MovingStrategy strategy) {
-        for (AbstractMovingFigur movingFigur : movingObjects) {
-            if (movingFigur.getObjectType() == type) {
-                Direction direction = strategy.getDirection(this, movingFigur);
-                moveFigurToDirection(movingFigur, direction);
+    public void moveMovableFigur( ObjectType type , MovingStrategy strategy ) {
+        for ( AbstractMovingFigur movingFigur : movingObjects ) {
+            if ( movingFigur.getObjectType( ) == type ) {
+                Direction direction = strategy.getDirection( this , movingFigur );
+                moveFigurToDirection( movingFigur , direction );
             }
         }
-        notifyAllListeners();
+        notifyAllListeners( );
     }
 
     @Override
-    public List<AbstractMovingFigur> getMovebleData() {
+    public List < AbstractMovingFigur > getMovebleData( ) {
         return movingObjects;
     }
 
     private Action lastAction = null;
 
     @Override
-    public void shootAllBullets () {
-        for (Bullet currentBullet : bulletList) {
-            int [] nextCoord = currentBullet.move(currentBullet.getShootingDirection());
-            int y = nextCoord[0];
-            int x = nextCoord[1];
-            AbstractFigur nextObject = getFigurByCoordinate(new Coordinate(x, y));
-            Action action = currentBullet.process(nextObject);
-            if (action == Action.SHOOT) {
-                if (getFigurByCoordinate(currentBullet.getCoordinate()).getObjectType() == ObjectType.BULLET) {
-                    setObjectByCoordinate(currentBullet.getCoordinate().getY(), currentBullet.getCoordinate().getX(), new Emptiness());
+    public void shootAllBullets( ) {
+        for ( Bullet currentBullet : bulletList ) {
+            int[] nextCoord = currentBullet.move( currentBullet.getShootingDirection( ) );
+            int y = nextCoord[ 0 ];
+            int x = nextCoord[ 1 ];
+            AbstractFigur nextObject = getFigurByCoordinate( new Coordinate( x , y ) );
+            Action action = currentBullet.process( nextObject );
+            if ( action == Action.SHOOT ) {
+                if ( getFigurByCoordinate( currentBullet.getCoordinate( ) ).getObjectType( ) == ObjectType.BULLET ) {
+                    setObjectByCoordinate( currentBullet.getCoordinate( ).getY( ) , currentBullet.getCoordinate( ).getX( ) , new Emptiness( ) );
                 }
-                setObjectByCoordinate(y, x, currentBullet);
-            } else if (action == Action.EAT_BOT) {
-                movingObjects.remove(nextObject);
-                if(Objects.requireNonNull(nextObject).getObjectType() == ObjectType.BOT) {
-                    data[nextObject.getCoordinate().getY()][nextObject.getCoordinate().getX()] = new Emptiness();
+                setObjectByCoordinate( y , x , currentBullet );
+            } else if ( action == Action.EAT_BOT ) {
+                movingObjects.remove( nextObject );
+                if ( Objects.requireNonNull( nextObject ).getObjectType( ) == ObjectType.BOT ) {
+                    data[ nextObject.getCoordinate( ).getY( ) ][ nextObject.getCoordinate( ).getX( ) ] = new Emptiness( );
                 }
-            } else if (action == Action.NONE) {
-                bulletList.remove(currentBullet);
-                data[currentBullet.getCoordinate().getY()][currentBullet.getCoordinate().getX()] = new Emptiness();
+            } else if ( action == Action.NONE ) {
+                bulletList.remove( currentBullet );
+                data[ currentBullet.getCoordinate( ).getY( ) ][ currentBullet.getCoordinate( ).getX( ) ] = new Emptiness( );
             }
         }
     }
 
-    private void moveFigurToDirection(AbstractMovingFigur movingFigur, Direction direction) {
-        int[] nextCoord = movingFigur.move(direction);
-        int y = nextCoord[0];
-        int x = nextCoord[1];
-        AbstractFigur nextObject = getFigurByCoordinate(new Coordinate(x, y));
-        Action action = movingFigur.process(nextObject);
-        player.setGameStatus(action);
-        AbstractFigur swapedFigur = new Emptiness();
-        switch (action) {
+    private void moveFigurToDirection( AbstractMovingFigur movingFigur , Direction direction ) {
+        int[] nextCoord = movingFigur.move( direction );
+        int y = nextCoord[ 0 ];
+        int x = nextCoord[ 1 ];
+        AbstractFigur nextObject = getFigurByCoordinate( new Coordinate( x , y ) );
+        Action action = movingFigur.process( nextObject );
+        player.setGameStatus( action );
+        AbstractFigur swapedFigur = new Emptiness( );
+        switch ( action ) {
             case WIN:
             case LOSE:
-                movingObjects.remove(player);
-              //  playerDAO.save(player);
-                if(movingFigur.getObjectType() == ObjectType.PLAYER){
-                    data[player.getCoordinate().getY()][player.getCoordinate().getX()] = new Emptiness();
+                movingObjects.remove( player );
+                //  playerDAO.save(player);
+                if ( movingFigur.getObjectType( ) == ObjectType.PLAYER ) {
+                    data[ player.getCoordinate( ).getY( ) ][ player.getCoordinate( ).getX( ) ] = new Emptiness( );
                     movingFigur = null;
                 }
-                nextObject = new Emptiness();
+                nextObject = new Emptiness( );
             case EAT_BOT:
-                movingObjects.remove(nextObject);
-                if(Objects.requireNonNull(movingFigur).getObjectType() == ObjectType.BOT_EATER) {
-                    data[movingFigur.getCoordinate().getY()][movingFigur.getCoordinate().getX()] = new Emptiness();
+                movingObjects.remove( nextObject );
+                if ( Objects.requireNonNull( movingFigur ).getObjectType( ) == ObjectType.BOT_EATER ) {
+                    data[ movingFigur.getCoordinate( ).getY( ) ][ movingFigur.getCoordinate( ).getX( ) ] = new Emptiness( );
                 }
-                nextObject = new Emptiness();
+                nextObject = new Emptiness( );
             case BOT_GOLD:
             case BOT_EATER_PLAYER:
                 swapedFigur = nextObject;
             case ADD_GOLD:
             case MOVE:
-                setObjectByCoordinate(Objects.requireNonNull(movingFigur).getCoordinate().getY(), movingFigur.getCoordinate().getX(), swapedFigur);
-                setObjectByCoordinate(y, x, movingFigur);
+                setObjectByCoordinate( Objects.requireNonNull( movingFigur ).getCoordinate( ).getY( ) , movingFigur.getCoordinate( ).getX( ) , swapedFigur );
+                setObjectByCoordinate( y , x , movingFigur );
         }
     }
 
